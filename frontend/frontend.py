44d9aa5f6b4996320b277eab40c13664e659cc95
lakshmipaswan352-dev <lakshmipaswan352@gmail.com>
2026-01-28 12:53:55 +0530

    Update frontend.py

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -8,12 +8,12 @@ while True:
         response = requests.get(backend_url)
 
         if response.status_code == 200:
-            print("Frontend received the following response from the backend:")
-            print(response.text)
+            print("Frontend received the following response from the backend:", flush=True)
+            print(response.text, flush=True)
         else:
-            print("Failed to communicate with the backend.")
+            print("Failed to communicate with the backend.", flush=True)
 
     except Exception as e:
-        print("Error connecting to backend:", e)
+        print("Error connecting to backend:", e, flush=True)
 
     time.sleep(10)
